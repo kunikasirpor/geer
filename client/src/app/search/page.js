d901218b9f405066408d0afce50cdddd898f8908
Kunika <157152440+kunikasirpor@users.noreply.github.com>
2025-07-20 23:28:10 +0530

    Update page.js

--- client/src/app/search/page.js
+++ client/src/app/search/page.js
@@ -1,12 +1,10 @@
-'use client'
-
-import { Suspense } from 'react';
-import SearchResults from './SearchResults';
+import { Suspense } from 'react'
+import SearchResults from './SearchResults'
 
 export default function Page() {
   return (
-    <Suspense fallback={<div className="text-center py-10">Loading search...</div>}>
+    <Suspense fallback={<div className="text-center py-10">Loading search results...</div>}>
       <SearchResults />
     </Suspense>
-  );
+  )
 }
